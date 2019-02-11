1ea4b8d3c607eb288b4c311d69f9845c4645420d
Iwancof <rock.iwancof@gmail.com>
2019-02-11 18:50:48 +0900

    あとはいつか(今日)

--- My_Neural_Network_4/Program.cs
+++ My_Neural_Network_4/Program.cs
@@ -4,9 +4,18 @@ namespace My_Neural_Network_4
 {
     class Program
     {
-        static void Main(string[] args) {
-            Neural n = new Neural(3,4,2);
+        static double[] ReplaceToOneZero(double[] inp,double[] s) {
+            double[] Result = new double[inp.Length];
+            for(int i = 0;i < Result.Length; i++) {
+                Result[i] = ((inp[i] >= s[i]) ? 1 : 0);
+            }
+            return Result;
+        }
 
+        static void Main(string[] args) {
+            Neural n = new Neural(3, 5, 2);
+
+            /*
             double[][] InputData = new double[][] {
                 new double[] { 0.0, 0.0, 0.0},
                 new double[] { 0.0, 0.0, 1.0},
@@ -27,6 +36,8 @@ namespace My_Neural_Network_4
                 new double[] { 1.0, 0.0},
                 new double[] { 1.0, 1.0},
             };
+            */
+            double[][] InputData = new double[32][];
             /*
             double[][] Answer = new double[][] {
                 new double[]{ 0.0 },
@@ -36,28 +47,57 @@ namespace My_Neural_Network_4
             };
             */
 
-            for(int i = 0;i < 10000; i++) {
-                for(int j = 0;j < 8; j++) {
-                    n.ForwardPropagation(InputData[j],Answer[j]);
+            for (int i = 0; i < 10000; i++) {
+                for (int j = 0; j < 8; j++) {
+                    n.ForwardPropagation(InputData[j], Answer[j]);
                     n.BackPropagation(InputData[j], Answer[j]);
                 }
                 //Console.WriteLine(n.GetError());
             }
 
-            for(int i = 0;i < 8; i++) {
+            double[,] Output = new double[8,2];
+            double[] Average = new double[2];
+
+            for (int i = 0; i < 8; i++) {
+                double[] Tmp = n.ForwardPropagation(InputData[i], Answer[i]);
+                for (int j = 0; j < 2; j++) {
+                    Output[i, j] = Tmp[j];
+                    Average[j] += Tmp[j];
+                }               
+            }
+
+            Average[0] /= 8;
+            Average[1] /= 8;
+
+
+
+            for (int i = 0;i < 8; i++) {
+                /*
                 foreach(double x in InputData[i]) {
                     Console.Write(x + ":");
                 }
+                */
+                /*
                 Console.WriteLine();
                 foreach (double x in Answer[i]) {
                     Console.Write(x + ":");
                 }
                 Console.WriteLine();
-                foreach (double x in n.ForwardPropagation(InputData[i],Answer[i])) {
-                    Console.Write(x + ":");                    
+                */
+                /*
+                for (int j = 0; j < 2; j++)
+                    Console.Write(Output[i, j] + ":");
+                Console.WriteLine();
+                */
+                /*
+                for (int j = 0; j < 2; j++)
+                    Console.Write((Output[i, j] > Average[j] ? 1 : 0) + ":"); 
+                Console.WriteLine("\n");
+                */
+                for(int j = 0;j < 2; j++) {
+                    Console.Write(Math.Abs(Answer[i][j] - Output[i, j]) + ":");
                 }
-                Console.WriteLine("\n\n\n");
-
+                Console.WriteLine();
             }
 
 
